39fbc7dfa3f5de7f1231864830219f2c8d1cdf70
Asier <asiturr@gmail.com>
2023-11-05 19:25:19 +0100

    0.1 inicio

--- app/src/main/java/com/example/zaldibar_patapol/MainActivity.kt
+++ app/src/main/java/com/example/zaldibar_patapol/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.example.zaldibar_patapol
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ProgressBar
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var progressBar: ProgressBar
+    private var progressStatus = 0
+    private val handler = Handler()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        progressBar = findViewById(R.id.progressBar)
+
+        // Inicia la animación
+        startProgressBarAnimation()
+    }
+
+    private fun startProgressBarAnimation() {
+        Thread {
+            while (progressStatus < 100) {
+                progressStatus += 1
+                handler.post {
+                    progressBar.progress = progressStatus
+                }
+                try {
+                    // Ajusta el tiempo de pausa para controlar la velocidad de la animación
+                    Thread.sleep(50)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
+            }
+            runOnUiThread {
+                cargarinicio()
+            }
+        }.start()
+    }
+
+    private fun cargarinicio() {
+        val intent = Intent(this, MapsActivity::class.java)
+        startActivity(intent)
     }
 }
