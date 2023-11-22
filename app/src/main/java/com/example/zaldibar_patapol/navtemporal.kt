013294940115348bed2ef8eaca45c65a19cdf934
ik_2dm3 <ik_2dm3@H015110.NET>
2023-11-22 09:22:32 +0100

    josu

--- app/src/main/java/com/example/zaldibar_patapol/navtemporal.kt
+++ app/src/main/java/com/example/zaldibar_patapol/navtemporal.kt
@@ -1,11 +1,26 @@
 package com.example.zaldibar_patapol
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.FragmentTransaction
+import androidx.navigation.Navigation
 
 class navtemporal : AppCompatActivity() {
+    private lateinit var birziklapen: Button
+    @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_navtemporal)
+
+        birziklapen = findViewById(R.id.birziklapenjolasa)
+            birziklapen.setOnClickListener {
+                val fragment = BirziklapenJolasa()
+                val transaction = supportFragmentManager.beginTransaction()
+                transaction.replace(R.id.fragment, fragment)
+                transaction.commit()
+            }
     }
 }
