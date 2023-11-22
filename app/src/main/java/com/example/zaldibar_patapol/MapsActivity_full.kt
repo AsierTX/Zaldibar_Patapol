2f37b598a2e410fa13cab2ac0d00d600b6f9c7c7
ik_2dm3 <ik_2dm3@H015110.NET>
2023-11-22 11:57:50 +0100

    mapa localicacion

--- app/src/main/java/com/example/zaldibar_patapol/MapsActivity_full.kt
+++ app/src/main/java/com/example/zaldibar_patapol/MapsActivity_full.kt
@@ -1,9 +1,13 @@
 package com.example.zaldibar_patapol
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -14,23 +18,33 @@ import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsActivity_full : AppCompatActivity(), OnMapReadyCallback {
 
+
+    //Objeto que usamos para definir todos los datos de la array
     data class MarkerInfo(
         val name: String,
         val coordinates: LatLng,
         val onClickFunction: () -> Unit
     )
-
-
+    //Array con las ubicaciones de los juegos
     val markerList = listOf(
-        MarkerInfo("Marcador1", LatLng(43.169689, -2.546189), ::onMarker1Click,),
-        MarkerInfo("Marcador2", LatLng(43.170217, -2.547697), ::onMarker2Click,),
-
+        MarkerInfo("San Martin Ermita", LatLng(43.169689, -2.546189), ::onMarker1Click,),
+        MarkerInfo("Bainuetxe-Ospital psikiatrikoa", LatLng(43.170217, -2.547697), ::onMarker2Click,),
+        MarkerInfo("Zaldibarko Pilotalekuak", LatLng(43.169050, -2.547347), ::onMarker3Click,),
+        MarkerInfo("Zaldibarko Dorrea", LatLng(43.168658, -2.547214), ::onMarker4Click,),
+        MarkerInfo("Santamañeko istripua", LatLng(43.168128, -2.545681), ::onMarker5Click,),
+        MarkerInfo("Zabortegia", LatLng(43.171058, -2.545683), ::onMarker6Click,),
+        MarkerInfo("Jaietako txopoa ", LatLng(43.170783, -2.545906), ::onMarker7Click,),
         )
 
 
 
 
     private lateinit var map: GoogleMap
+
+    companion object{
+        const val REQUEST_CODE_LOCATION = 0
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps_full)
@@ -43,11 +57,53 @@ class MapsActivity_full : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
     }
 
+    //Comprueba si el permiso esta dado o no
+    private fun isLocationPermissionGranted() = ContextCompat.checkSelfPermission(
+        this,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED
+
+    private fun enableLocation(){
+        if(!::map.isInitialized)return
+        if(isLocationPermissionGranted()){
+
+            map.isMyLocationEnabled = true
+        }else{
+            requestLocationPermission()
+        }
+    }
+
+    private fun requestLocationPermission(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+            Toast.makeText(this, "Acepta los permisos", Toast.LENGTH_SHORT).show()
+        }else{
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),REQUEST_CODE_LOCATION)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            REQUEST_CODE_LOCATION -> if(grantResults.isNotEmpty() && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                map.isMyLocationEnabled=true
+            }else{
+                Toast.makeText(this, "Ve a ajustes a aceptar los permisos", Toast.LENGTH_SHORT).show()
+            }else ->{}
+        }
+    }
+
+
+
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
+        enableLocation()
 
         // Zoom en coordenadas específicas de zaldibar
-        val location = LatLng(43.1709413,-2.5466649) // Nueva York, por ejemplo
+        val location = LatLng(43.1709413,-2.5466649) // coordenadas de Zaldibar
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 16f))
 
 
@@ -77,11 +133,35 @@ class MapsActivity_full : AppCompatActivity(), OnMapReadyCallback {
 
     private fun onMarker1Click() {
         Toast.makeText(this, "Clic en Marcador 1", Toast.LENGTH_SHORT).show()
-        // Puedes agregar más acciones según tus necesidades
+
     }
 
     private fun onMarker2Click() {
         Toast.makeText(this, "Clic en Marcador 2", Toast.LENGTH_SHORT).show()
-        // Puedes agregar más acciones según tus necesidades
+
     }
+
+    private fun onMarker3Click() {
+        Toast.makeText(this, "Clic en Marcador 3", Toast.LENGTH_SHORT).show()
+
+    }
+    private fun onMarker4Click() {
+        Toast.makeText(this, "Clic en Marcador 4", Toast.LENGTH_SHORT).show()
+
+    }
+    private fun onMarker5Click() {
+        Toast.makeText(this, "Clic en Marcador 5", Toast.LENGTH_SHORT).show()
+
+    }
+    private fun onMarker6Click() {
+        Toast.makeText(this, "Clic en Marcador 6", Toast.LENGTH_SHORT).show()
+
+    }
+    private fun onMarker7Click() {
+        Toast.makeText(this, "Clic en Marcador 7", Toast.LENGTH_SHORT).show()
+
+    }
+
+
+
 }
