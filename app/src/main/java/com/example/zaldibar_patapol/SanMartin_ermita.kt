62e84d1fad5d7bc29c6efeff5c19fa2dad875d0e
014 <ikersiles35@gmail.com>
2023-11-28 13:02:16 +0100

    Diseño juego 1 v.01

---
+++ app/src/main/java/com/example/zaldibar_patapol/SanMartin_ermita.kt
@@ -0,0 +1,11 @@
+package com.example.zaldibar_patapol
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SanMartin_ermita : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_san_martin_ermita)
+    }
+}
